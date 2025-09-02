970d0c84392aa7d7fcc018508f986f94adfd3af8
Mohamed Haji <hajimuhammed27@gmail.com>
2025-09-02 19:59:57 +0000

    entferne die Offline Funktionalität

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { LanguageProvider } from '@/hooks/use-translation';
 import { Inter } from 'next/font/google'
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Zeit Meister',
@@ -20,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="de" dir="ltr" className={`${inter.variable}`}>
       <head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
@@ -28,19 +26,6 @@ export default function RootLayout({
           {children}
           <Toaster />
         </LanguageProvider>
-        <Script id="service-worker-registration">
-          {`
-            if ('serviceWorker' in navigator) {
-              window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js').then(registration => {
-                  console.log('SW registered: ', registration);
-                }).catch(registrationError => {
-                  console.log('SW registration failed: ', registrationError);
-                });
-              });
-            }
-          `}
-        </Script>
       </body>
     </html>
   );
